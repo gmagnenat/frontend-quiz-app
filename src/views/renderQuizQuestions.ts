bdfcb561b4a7fd7bf5592cfd38e62a311606f212
Gwenaël Magnenat <g.magnenat@gmail.com>
2024-11-08 16:51:18 +0100

    Add renderQuizQuestion (not finished)

--- src/views/renderQuizQuestions.ts
+++ src/views/renderQuizQuestions.ts
@@ -1,1 +1,16 @@
-export function renderQuizQuestions() {}
+import { getCurrentQuiz } from "../state/state";
+
+const quizContainer: HTMLElement | null = document.querySelector(
+  ".hero__quiz-container"
+);
+
+export function renderQuizQuestions() {
+  const currentQuiz = getCurrentQuiz();
+  if (quizContainer) {
+    quizContainer.innerHTML = `
+      <div class="hero__quiz">
+        <h1>${currentQuiz?.title}</h1>
+      </div>
+    `;
+  }
+}
