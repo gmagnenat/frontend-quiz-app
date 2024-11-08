96eefa20730741ada4a830a65f50b5aef4a1930c
Gwenaël Magnenat <g.magnenat@gmail.com>
2024-11-08 15:48:25 +0100

    Add jsdoc

--- src/utils/loadData.ts
+++ src/utils/loadData.ts
@@ -1,6 +1,10 @@
 import data from "../data/data.json";
 import { Quiz } from "../types/quizTypes";
 
+/**
+ * Load quiz data from JSON file
+ * @returns an array of quiz
+ */
 export function loadData(): Quiz[] {
   return data.quizzes as Quiz[];
 }
