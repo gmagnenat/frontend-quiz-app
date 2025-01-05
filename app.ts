6f5705b687da580a8efa9eb91444da00b7f569c3
Gwenaël Magnenat <g.magnenat@gmail.com>
2025-01-05 14:18:35 +0100

    Add function to load quiz from url.

--- app.ts
+++ app.ts
@@ -1,19 +1,58 @@
 import { renderApp } from "./src/views/renderApp";
-import { setCurrentTheme } from "./src/state/state";
+import {
+  setCurrentTheme,
+  setCurrentQuiz,
+  setCurrentView,
+} from "./src/state/state";
+import { selectQuiz } from "./src/utils/selectQuiz";
 
-renderApp();
+function loadQuizFromUrl(): void {
+  const path = window.location.pathname.toLowerCase();
+  const quizName = path.substring(1).replace(/\b\w/g, (c) => c.toUpperCase());
 
-// Theme toggle
-const lightMode = document.getElementById("light-mode") as HTMLInputElement;
-const darkMode = document.getElementById("dark-mode") as HTMLInputElement;
+  if (quizName) {
+    const quiz = selectQuiz(quizName.toUpperCase());
+    if (quiz) {
+      setCurrentQuiz(quiz);
+      setCurrentView("quiz");
+    } else {
+      console.error(`Quiz "${quizName}" not found.`);
+    }
+  }
+}
 
-// Apply saved theme from localStorage
-const savedTheme = localStorage.getItem("theme") || "light";
-document.documentElement.setAttribute("data-theme", savedTheme);
-lightMode.checked = savedTheme === "light";
-darkMode.checked = savedTheme === "dark";
+function initApp(): void {
+  const savedTheme = localStorage.getItem("theme") || "light";
+  document.documentElement.setAttribute("data-theme", savedTheme);
 
-// Update theme
+  const lightMode = document.getElementById("light-mode") as HTMLInputElement;
+  const darkMode = document.getElementById("dark-mode") as HTMLInputElement;
+  lightMode.checked = savedTheme === "light";
+  darkMode.checked = savedTheme === "dark";
+
+  loadQuizFromUrl();
+
+  lightMode?.addEventListener("keydown", (e) => {
+    if (e.key === "ArrowRight") {
+      updateTheme("dark");
+    }
+  });
+  darkMode?.addEventListener("keydown", (e) => {
+    if (e.key === "ArrowLeft") {
+      updateTheme("light");
+    }
+  });
+
+  lightMode?.addEventListener("click", () => {
+    updateTheme("light");
+  });
+  darkMode?.addEventListener("click", () => {
+    updateTheme("dark");
+  });
+
+  renderApp();
+}
+
 type CurrentTheme = "light" | "dark";
 
 /**
@@ -26,6 +65,4 @@ function updateTheme(theme: CurrentTheme): void {
   setCurrentTheme(theme);
 }
 
-// Event listeners to update theme
-lightMode.addEventListener("change", () => updateTheme("light"));
-darkMode.addEventListener("change", () => updateTheme("dark"));
+document.addEventListener("DOMContentLoaded", initApp);
